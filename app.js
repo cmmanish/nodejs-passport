1ce3bbff5bc225fd393b952284fcc4a5f3d34a0f
Manish Madhusoodan <mmadhusoodan@marinsoftware.com>
2014-08-23 12:39:28 -0700

    routes split

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var expressSession = require('express-Session');
 
 var passport = require('passport');
 var passportLocal = require('passport-local');
-var passportHttp = require('passport-http')
+var passportHttp = require('passport-http');
+
+// by default, brings in routes/index.js
+
+var routes = require('./routes/index')
 
 var app = new express();
 app.set('view engine', 'ejs'); 
@@ -21,14 +25,7 @@ app.use(expressSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new passportLocal.Strategy(function(username, password, done){
-	//pretend this is read database! 
-	if (username == password){
-		done(null,{id:username, name:username});
-	} else {
-		done(null, null);
-	}
-}));
+passport.use(new passportLocal.Strategy(routes.verifyUser));
 
 passport.serializeUser(function(user, done){
 	done(null, user.id);
@@ -38,19 +35,8 @@ passport.deserializeUser(function(id, done){
 	done(null, { id:id, name:id });
 });
 
-//
-function ensureAuthenticated(req, res, next){
-	if (req.isAuthenticated()){
-		next();
-	}else {
-		//res.redirect('/login')
-		res.send(403)
-		//res.status(403).end()
-	}
 
-}
-
-//routes are here
+//routes
 app.get('/', function(req, res){
 	res.render('index', {
 		isAuthenticated: req.isAuthenticated(),
@@ -59,7 +45,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/login', function(req, res){
-		res.render('login');
+		res.render('login')
 });
 
 app.get('/logout', function(req, res){
@@ -71,13 +57,15 @@ app.post('/login', passport.authenticate('local'), function(req,res){
 	res.redirect('/')
 });
 
-app.get('/api/data', ensureAuthenticated, function(req,res){
+app.get('/api/data', routes.ensureAuthenticated, function(req,res){
 	res.json([
 		{value: 'Sachin'},
 		{value: 'Rahul'},
 		{value: 'Laxman'}
 	]);
 });
+
+
 
 //setting port and listening
 var port = process.env.PORT || 8888 ;
--- dataRetrival.js
+++ dataRetrival.js
@@ -7,12 +7,12 @@ var url = 'https://api.humanapi.co/v1/human/?access_token=demo'
 
 request.get('http://red.marinsoftware.com/').auth('search@powpowsports.com', 'marin2010', false);
 
-// request(url, function(error, response, body) {
-//   var obj = JSON.parse(body);
-//   console.log(obj);
+request(url, function(error, response, body) {
+  var obj = JSON.parse(body);
+  console.log(obj);
   
-//   console.log(response.body);
-// });
+  console.log(response.body);
+});
 
 
 
